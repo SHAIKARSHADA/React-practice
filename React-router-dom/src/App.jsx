81e2cc1f314c8959d08937a7bf3eb5d774482b05
Shaik <shaikarshad1792004@gmail.com>
2024-06-26 23:56:39 +0530

    added useContext

--- React-router-dom/src/App.jsx
+++ React-router-dom/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { CountContext } from "./components/context";
 
 
 
@@ -7,24 +8,37 @@ function App() {
 
   const [ count, setCount ] = useState(0);
 
+
+  //wrap anyone that wants to use the teleported value inside a provider
   return(
     <>
-    <Count/>
-    <Button count={count} setCount={setCount}/>
+    <CountContext.Provider value={count}> 
+      <Count setCount={setCount}/>
+    </CountContext.Provider>
+    
     </>
   ) 
 }
 
 
-function Count({count}) {  
+function Count({setCount}) {  
   return <>
-    {count}
+    <CountRenderer />
+    <Buttons  setCount={setCount}/>
   </>
 
 
 }
 
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <>
+    {count}
+  </>
+}
+
 function Buttons({count, setCount}) {
+  const count = useContext(CountContext);
   return(<>
   <button onClick={() => { setCount(count+1)}}>
     Increase
