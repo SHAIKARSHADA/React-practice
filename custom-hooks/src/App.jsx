b92dcea6c5311451aaa984d1d5570cb3aa816bd4
Shaik <shaikarshad1792004@gmail.com>
2024-07-05 21:47:31 +0530

    used MouseLocation.jsx in app.jsx

--- custom-hooks/src/App.jsx
+++ custom-hooks/src/App.jsx
@@ -1,14 +1,16 @@
 import { useIsOnline } from "./hooks/useIsOnline"
+import { useMouselocation } from "./useMouselocation"
 
 export default function App() {
   
   const value = useIsOnline()
-  
+  const mousevalue = useMouselocation();
   return (
     <>
     {value}
     <div>
      {value ? "online, YaY!!!!" : "offline, YaY!!!!!"}
+     {"your mouse location is x axis : " + mousevalue.x+ "y axis : " + mousevalue.y}
     </div>
     </>
   )
